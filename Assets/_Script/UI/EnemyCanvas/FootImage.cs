bd6e837b17c6a35fb8839aa96ae793711a019ce5
Pureria <117608289+Pureria@users.noreply.github.com>
2023-06-27 18:04:41 +0900

    6/27
    敵の足音の可視化部分の作成（未完成

---
+++ Assets/_Script/UI/EnemyCanvas/FootImage.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FootImage : MonoBehaviour
+{
+    private Image footImage;
+    [SerializeField]
+    private float animationTime;
+    [SerializeField]
+    private Vector3 maxScale;
+    [SerializeField]
+    private bool debugFlg;
+
+    private bool isFootStep;
+    private bool isFootStepStop;
+
+    private Vector3 pos;
+
+    private float startTime;
+    private float nowTime;
+    private void Start()
+    {
+        footImage = GetComponent<Image>();
+        isFootStep = false;
+        isFootStepStop = false;
+        nowTime = 0.0f;
+
+        footImage.rectTransform.localScale = Vector3.zero;
+        footImage.color = new Color(footImage.color.r, footImage.color.g, footImage.color.b, 0.0f);
+    }
+
+    private void Update()
+    {
+        DebugCheck();
+        nowTime = Time.time;
+        if(isFootStep)
+        {
+            Vector3 sc = new Vector3(AnimationValue(startTime,startTime + animationTime,0.0f,maxScale.x,nowTime),
+                                     AnimationValue(startTime, startTime + animationTime, 0.0f, maxScale.y, nowTime),
+                                     AnimationValue(startTime, startTime + animationTime, 0.0f, maxScale.z, nowTime));
+            float a = AnimationValue(startTime, startTime + animationTime, 1.0f, 0.0f, nowTime);
+            footImage.rectTransform.localScale = sc;
+            footImage.color = new Color(footImage.color.r, footImage.color.g, footImage.color.b, a);
+
+            if(nowTime >= startTime + animationTime)
+            {
+                footImage.rectTransform.localScale = Vector3.zero;
+                footImage.color = new Color(footImage.color.r, footImage.color.g, footImage.color.b, 0);
+                startTime = Time.time;
+            }
+        }
+    }
+
+    public void StartFootStep()
+    {
+        isFootStep = true;
+        startTime = Time.time;
+    }
+    public void StopFootStep() => isFootStepStop = true;
+
+    public void SetPosition(Vector3 pos) { this.pos = pos; }
+
+    private float AnimationValue(float startTime,float endTime,float startKey,float endKey,float nowTime)
+    {
+        float ret = 0.0f;
+        float t = endTime - startTime;
+        float k = endKey - startKey;
+        if(k >= 0)
+        {
+            float key = k / t;
+            t = nowTime - startTime;
+            ret = key * t;
+        }
+        else
+        {
+            //TODO::StartKeyÇ™EndKeyÇÊÇËÇ‡è¨Ç≥Ç©Ç¡ÇΩèÍçáÇÃèàóù
+        }
+
+        return ret;
+    }
+
+    private void DebugCheck()
+    {
+        if(debugFlg)
+        {
+            if (!isFootStep)
+                StartFootStep();
+        }
+        else
+        {
+            if (isFootStep && !isFootStepStop)
+                StopFootStep();
+        }
+    }
+}
