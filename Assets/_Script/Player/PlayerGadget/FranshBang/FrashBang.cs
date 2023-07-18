4aa1aac9172d599b7dcda6fb3b14dfb6d79f3a46
Pureria <117608289+Pureria@users.noreply.github.com>
2023-07-18 18:16:43 +0900

    7/18
    スクリプトの編集

--- Assets/_Script/Player/PlayerGadget/FranshBang/FrashBang.cs
+++ Assets/_Script/Player/PlayerGadget/FranshBang/FrashBang.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class FrashBang : MonoBehaviour
 {
-    
+
+    [SerializeField]
+    FrashBangData frashBangData;
+
+    private float startTime;
+    private void Start()
+    {
+        startTime = Time.time;
+    }
+
+    private void Update()
+    {
+        
+    }
 }
