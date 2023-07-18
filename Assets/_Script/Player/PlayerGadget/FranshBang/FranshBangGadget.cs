9225bccc2b45cece5dc765367cbd296f5f5e2e65
Pureria <117608289+Pureria@users.noreply.github.com>
2023-07-18 18:15:52 +0900

    7/18
    ガジェット
    ・FrashbangGadgetスクリプトの作成
    ・FrashBangスクリプトの作成
    ・FrashBandDataスクリプトの作成
    
    全体
    ・重くなっていた処理の最適化
    
    全体的に重くなっていた処理を見直した
    
    フラッシュバンガジェットの作成
    プレイヤー側でガジェットを選択できるように作成
    まだフラッシュバンのメインスクリプトを作成できていないのとプレイヤー、敵、味方NPCにスタンが未実装

--- Assets/_Script/Player/PlayerGadget/FranshBang/FranshBangGadget.cs
+++ Assets/_Script/Player/PlayerGadget/FranshBang/FranshBangGadget.cs
@@ -9,11 +9,36 @@ public class FranshBangGadget : GadgetBase
     [SerializeField]
     private GameObject frashBangPrefab;
 
+    [SerializeField]
+    private int maxFlafhbangCount = 0;
+
+    [SerializeField]
+    private float debugThrowPower = 5.0f;
+    [SerializeField]
+    private Vector3 debugTorque = Vector3.zero;
+
+    [SerializeField]
+    private bool debugThrow = false;
+
     private int nowHaveCount;
 
     public void Start()
     {
         //nowHaveCountを最大値に設定
+        nowHaveCount = maxFlafhbangCount;
+    }
+
+    public void Update()
+    {
+        if (debugThrow)
+        {
+            debugThrow = false;
+            GameObject flashBang = Instantiate(frashBangPrefab, this.gameObject.transform.position, Quaternion.identity);
+            Rigidbody fbRB = flashBang.GetComponent<Rigidbody>();
+            Vector3 throwVec = flashBang.transform.forward * debugThrowPower;
+            fbRB.velocity = throwVec;
+            fbRB.AddTorque(debugTorque);
+        }
     }
 
     public override void UseGadget()
@@ -27,6 +52,12 @@ public class FranshBangGadget : GadgetBase
         }
 
         //ガジェットを前方に投げる処理
+        nowHaveCount--;
+        GameObject flashBang = Instantiate(frashBangPrefab, this.gameObject.transform.position, Quaternion.identity);
+        Rigidbody fbRB = flashBang.GetComponent<Rigidbody>();
+        Vector3 throwVec = this.transform.forward * debugThrowPower;
+        fbRB.velocity = throwVec;
+        fbRB.AddTorque(debugTorque);
     }
 
     public override void EndGadget()
